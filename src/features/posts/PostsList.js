fbcabb57976b3ed8283df3239cd2fae81f41d651
hkmusameh01 <abdalhakem1226@gmail.com>
2022-12-06 22:15:07 +0200

    just try to use plan js objects and sync code without using thunks

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
+const PostsList = () => {
+  // Be carfull => here is the /posts/ not allow
+  // to be mutated so make a copy and mutate
+  // the copy no the original
+  const posts = useSelector((state) => state.posts)
+
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderPosts = orderedPosts.map((post) => (
+    <article key={post.id} className="post-excerpt">
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  ))
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderPosts}
+    </section>
+  )
+}
+
+export default PostsList
---
+++ src/features/posts/SinglePostPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link, useParams } from 'react-router-dom'
+
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
+const SinglePostPage = () => {
+  const { postId } = useParams()
+
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postId)
+  )
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
+        <Link to={`/editPost/${postId}`} className="button">
+          Edit Post
+        </Link>
+      </article>
+    </section>
+  )
+}
+
+export default SinglePostPage
